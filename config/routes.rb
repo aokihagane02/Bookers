e642405ddd0323b1b2552029fb52d6b407421ab1
EC2 Default User <ec2-user@ip-172-31-84-175.ec2.internal>
2021-08-09 12:52:27 +0000

    [Update]views

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   get '/' => 'homes#top'
 
-  get 'blogs/index' => 'blogs#index'
-  get 'blogs/:id' => 'blogs#show'
-  get 'blogs/:id/edit' => 'blogs#edit'
-  post 'blogs/create' => 'blogs#create'
-  patch 'blogs/:id' => 'blogs#update'
-  delete 'blogs/:id' => 'blogs#destroy'
+  get 'books' => 'books#index'
+  get 'books/:id' => 'books#show'
+  get 'books/:id/edit' => 'books#edit'
+  post 'books/create' => 'books#create'
+  patch 'books/:id' => 'books#update'
+  delete 'books/:id' => 'books#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
